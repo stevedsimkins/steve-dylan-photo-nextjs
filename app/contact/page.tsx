f7ab2bce15c36c87724d1be851d19d532ecf970e
Steve <stevedsimkins@gmail.com>
2023-04-18 21:43:10 -0400

    updated contact weight

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,10 +4,12 @@ import { EnvelopeIcon, CameraIcon } from "@heroicons/react/24/solid";
 export default function Contact() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
-      <h1 className="font-ebGaramond text-5xl my-10">Contact</h1>
+      <h1 className="font-ebGaramond text-5xl my-10 font-bold">Contact</h1>
       <div className="flex flex-col gap-5">
-        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="mailto:stevedsimkins@gmail.com"><EnvelopeIcon className="h-6 w-6" />Email</Link>
-        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="https://instagram.com/stevedylanphoto"><CameraIcon className="w-6 h-6" />Instagram</Link>
+        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="mailto:stevedsimkins@gmail.com">
+          <EnvelopeIcon className="h-6 w-6" />Email</Link>
+        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="https://instagram.com/stevedylanphoto">
+          <CameraIcon className="w-6 h-6" />Instagram</Link>
       </div>
     </div>
   )
