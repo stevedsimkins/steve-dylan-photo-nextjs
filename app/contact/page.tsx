36863ba35651fd5652cdf79205c6e0ef62e49adf
Steve <stevedsimkins@gmail.com>
2023-04-24 14:59:25 -0400

    that'll work

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
-import { EnvelopeIcon, CameraIcon } from "@heroicons/react/24/outline";
 
 export default function Contact() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-5 font-ebGaramond">
       <h1 className="font-ebGaramond text-5xl my-10 font-bold">Contact</h1>
-      <div className="flex flex-col gap-5">
-        <Link className="font-ebGaramond text-2xl flex items-center gap-2" href="mailto:stevedsimkins@gmail.com">
-          <EnvelopeIcon className="h-10 w-10 bg-gray-700 hover:bg-gray-400 p-2 rounded text-white" />Email</Link>
-        <Link className="font-ebGaramond text-2xl flex items-center gap-2" href="https://instagram.com/stevedylanphoto">
-          <CameraIcon className="h-10 w-10 bg-gray-700 hover:bg-gray-400 p-2 rounded text-white" />Instagram</Link>
+      <div className="flex flex-col items-center gap-5">
+        <div className="flex gap-1">
+          <p>email</p>
+          <p>&bull;</p>
+          <Link className="font-ebGaramond flex items-center gap-2 hover:underline" href="mailto:stevedsimkins@gmail.com">hello@stevedylanphoto.com</Link>
+        </div>
+        <div className="flex gap-1">
+          <p>instagram</p>
+          <p>&bull;</p>
+          <Link className="font-ebGaramond flex items-center gap-2 hover:underline" href="https://instagram.com/stevedylanphoto">@stevedylanphoto</Link>
+        </div>
       </div>
     </div>
   )
