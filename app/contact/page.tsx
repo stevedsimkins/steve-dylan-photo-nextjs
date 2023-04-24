0c068c0ed754219bb5abfbda0b7b8fe88670b285
Steve <stevedsimkins@gmail.com>
2023-04-24 13:04:45 -0400

    added some contact changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import { EnvelopeIcon, CameraIcon } from "@heroicons/react/24/solid";
+import { EnvelopeIcon, CameraIcon } from "@heroicons/react/24/outline";
 
 export default function Contact() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
       <h1 className="font-ebGaramond text-5xl my-10 font-bold">Contact</h1>
       <div className="flex flex-col gap-5">
-        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="mailto:stevedsimkins@gmail.com">
-          <EnvelopeIcon className="h-6 w-6" />Email</Link>
-        <Link className="font-ebGaramond text-2xl hover:underline flex items-center gap-2" href="https://instagram.com/stevedylanphoto">
-          <CameraIcon className="w-6 h-6" />Instagram</Link>
+        <Link className="font-ebGaramond text-2xl flex items-center gap-2" href="mailto:stevedsimkins@gmail.com">
+          <EnvelopeIcon className="h-10 w-10 bg-gray-700 hover:bg-gray-400 p-2 rounded text-white" />Email</Link>
+        <Link className="font-ebGaramond text-2xl flex items-center gap-2" href="https://instagram.com/stevedylanphoto">
+          <CameraIcon className="h-10 w-10 bg-gray-700 hover:bg-gray-400 p-2 rounded text-white" />Instagram</Link>
       </div>
     </div>
   )
